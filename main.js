13b5183fead3a36dd87593db1074ab1d8337b32e
Nuclear One <ajithsnair10@gmail.com>
2022-02-20 12:13:15 +0530

    collect data from form

---
+++ main.js
@@ -0,0 +1,20 @@
+// sessionStorage.setItem('name','bob')
+// localStorage.setItem('name','kyle')
+
+// console.log(sessionStorage.getItem('name'))
+
+// document.cookie='name=bob';
+// console.log(document.cookie);
+
+
+let btn=document.querySelector('.btn')
+let namebox=document.querySelector('#name')
+let emailbox=document.querySelector('#email')
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    console.log(namebox.value);
+    console.log(emailbox.value);
+    localStorage.clear();
+    localStorage.setItem('Name', namebox.value);
+    localStorage.setItem('Email', emailbox.value);
+})
