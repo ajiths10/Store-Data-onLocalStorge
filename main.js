47c238dffd9800eec1b6e69c492eb589644f1f3d
Nuclear One <ajithsnair10@gmail.com>
2022-02-25 10:08:41 +0530

    final

--- main.js
+++ main.js
@@ -7,38 +7,34 @@
 // console.log(document.cookie);
 
 
-// let btn=document.querySelector('.btn')
-// let namebox=document.querySelector('#name')
-// let emailbox=document.querySelector('#email')
+let btn=document.querySelector('.btn')
+let namebox=document.querySelector('#name')
+let emailbox=document.querySelector('#email')
 
-// btn.addEventListener('click',(e)=>{
-//     e.preventDefault();
-//     console.log(namebox.value);
-//     console.log(emailbox.value);
-//     localStorage.clear();
-//     localStorage.setItem('Name', namebox.value);
-//     localStorage.setItem('Email', emailbox.value);
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let myobj={
+        name:namebox.value,
+        age:emailbox.value
+    };
+    let myobj_serialized=JSON.stringify(myobj);
+    console.log(myobj_serialized)
+    localStorage.setItem("myobj",myobj_serialized);
+    //console.log(localStorage);
+    let myobj_deserialized=JSON.parse(localStorage.getItem("myobj"));
+    console.log(myobj_deserialized);
+    
+    //make new
+    let cent=document.getElementById('newdiv');
+    
+    let ele=document.createElement('li');
+    ele.appendChild(document.createTextNode(`Name = ${myobj_deserialized.name}`));
+    //document.body.appendChild(ele)
+    cent.appendChild(ele)
+    let ag=document.createElement('li');
+    ag.appendChild(document.createTextNode(`Age = ${myobj_deserialized.age}`));
+    cent.appendChild(ag);
 
-// })
 
-let myobj={
-    name:"Ajith",
-    age:24
-};
-let myobj_serialized=JSON.stringify(myobj);
-console.log(myobj_serialized)
-localStorage.setItem("myobj",myobj_serialized);
-//console.log(localStorage);
-let myobj_deserialized=JSON.parse(localStorage.getItem("myobj"));
-console.log(myobj_deserialized);
+})
 
-//make new
-let cent=document.getElementById('newdiv');
-
-let ele=document.createElement('li');
-ele.appendChild(document.createTextNode(`Name = ${myobj_deserialized.name}`));
-//document.body.appendChild(ele)
-cent.appendChild(ele)
-let ag=document.createElement('li');
-ag.appendChild(document.createTextNode(`Age = ${myobj_deserialized.age}`));
-cent.appendChild(ag);
