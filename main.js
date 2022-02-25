d530a7f593db1e48a3bb42a29aee1aad257a8e24
Nuclear One <ajithsnair10@gmail.com>
2022-02-25 09:58:04 +0530

    details display in frontend

--- main.js
+++ main.js
@@ -7,15 +7,35 @@
 // console.log(document.cookie);
 
 
-let btn=document.querySelector('.btn')
-let namebox=document.querySelector('#name')
-let emailbox=document.querySelector('#email')
-btn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    console.log(namebox.value);
-    console.log(emailbox.value);
-    localStorage.clear();
-    localStorage.setItem('Name', namebox.value);
-    localStorage.setItem('Email', emailbox.value);
-    
-})
+// let btn=document.querySelector('.btn')
+// let namebox=document.querySelector('#name')
+// let emailbox=document.querySelector('#email')
+
+// btn.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     console.log(namebox.value);
+//     console.log(emailbox.value);
+//     localStorage.clear();
+//     localStorage.setItem('Name', namebox.value);
+//     localStorage.setItem('Email', emailbox.value);
+
+// })
+
+let myobj={
+    name:"Ajith",
+    age:24
+};
+let myobj_serialized=JSON.stringify(myobj);
+console.log(myobj_serialized)
+localStorage.setItem("myobj",myobj_serialized);
+//console.log(localStorage);
+let myobj_deserialized=JSON.parse(localStorage.getItem("myobj"));
+console.log(myobj_deserialized);
+
+//make new
+let ele=document.createElement('h2');
+ele.appendChild(document.createTextNode(`Name = ${myobj_deserialized.name}`));
+document.body.appendChild(ele)
+let ag=document.createElement('h2');
+ag.appendChild(document.createTextNode(`Age = ${myobj_deserialized.age}`));
+document.body.appendChild(ag);
